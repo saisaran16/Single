b9f42a1b1c9f1a7109992414918e747c0cbd4aca
saisaran16 <nsvvsaisaran@gmail.com>
2020-06-29 14:35:55 +0530

    Add files via upload

---
+++ backup-settings/bpims-settings.js
@@ -0,0 +1,6 @@
+exports.client_id = "ZTE0NjYzYzctODcyZi00";
+exports.client_secret = "ODNmOWU1MjktZGYyNi00x";
+exports.authorization_url = "https://prepiam.toronto.ca.ibm.com/idaas/oidc/endpoint/default/authorize";
+exports.token_url = "https://prepiam.toronto.ca.ibm.com/idaas/oidc/endpoint/default/token";
+exports.issuer_id = "https://prepiam.toronto.ca.ibm.com";
+exports.callback_url = "https://localhost:9443";      
---
+++ backup-settings/preprod-blueid-settings.js
@@ -0,0 +1,6 @@
+exports.client_id = "NTEzNGEwZDAtOWRmNi00";
+exports.client_secret = "NjBjMjc0YjgtODg3ZC00";
+exports.authorization_url = "https://prepiam.toronto.ca.ibm.com/idaas/oidc/endpoint/default/authorize";
+exports.token_url = "https://prepiam.toronto.ca.ibm.com/idaas/oidc/endpoint/default/token";
+exports.issuer_id = "https://prepiam.toronto.ca.ibm.com";
+exports.callback_url = "https://localhost:9443/auth/sso/callback";      
