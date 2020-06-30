1e9f9f36407f2d4aa80d4463ed4860e83f89a273
saisaran16 <nsvvsaisaran@gmail.com>
2020-06-30 19:03:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
-exports.client_id = "72324357-4e7f-46be-98b4-a15c0ecbded9";
-exports.client_secret = "d65727d3-e51d-4b9c-84ef-2410846f3344";
-exports.authorization_url = "https://w3id.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/authorize";
-exports.token_url = "https://w3id.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/token";
-exports.issuer_id = "https://w3id.sso.ibm.com/isam";
-exports.callback_url = "https://localhost:9443/auth/sso/callback";      
+exports.client_id = "YzU0YmRiYjYtNzEwNi00";
+exports.client_secret = "YTIwZDNmNWYtNzZiYS00";
+exports.authorization_url = "https://preprod.login.w3.ibm.com/oidc/endpoint/default/authorize";
+exports.token_url = "https://preprod.login.w3.ibm.com/oidc/endpoint/default/token";
+exports.issuer_id = "https://preprod.login.w3.ibm.com/oidc/endpoint/default";
+exports.callback_url = "https://divya-git-sso-default.mycluster-dal10-b-550403-23d856d630cf08991a05ca754c06f1ff-0000.us-south.containers.appdomain.cloud/auth/sso/callback";      
