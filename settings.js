88613b8ea47746243489d58c4330d4193d114898
saisaran16 <nsvvsaisaran@gmail.com>
2020-07-01 14:40:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 exports.client_id = "YzU0YmRiYjYtNzEwNi00";
-exports.client_secret = "YTIwZDNmNWYtNzZiYS00";
+exports.client_secret = "ZGE4MTdlZTUtMWQ3MS00";
 exports.authorization_url = "https://preprod.login.w3.ibm.com/oidc/endpoint/default/authorize";
 exports.token_url = "https://preprod.login.w3.ibm.com/oidc/endpoint/default/token";
 exports.issuer_id = "https://preprod.login.w3.ibm.com/oidc/endpoint/default";
