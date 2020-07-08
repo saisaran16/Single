3996948e3d6ecc9537b0f856c146ab070738b6d4
saisaran16 <nsvvsaisaran@gmail.com>
2020-07-08 12:06:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,4 +3,4 @@ exports.client_secret = "ZGE4MTdlZTUtMWQ3MS00";
 exports.authorization_url = "https://preprod.login.w3.ibm.com/oidc/endpoint/default/authorize";
 exports.token_url = "https://preprod.login.w3.ibm.com/oidc/endpoint/default/token";
 exports.issuer_id = "https://preprod.login.w3.ibm.com/oidc/endpoint/default";
-exports.callback_url = "https://divya-git-sso-default.mycluster-dal10-b-550403-23d856d630cf08991a05ca754c06f1ff-0000.us-south.containers.appdomain.cloud/auth/sso/callback";      
+exports.callback_url = "https://hello-world12345asdfg.w3ibm.mybluemix.net/";      
