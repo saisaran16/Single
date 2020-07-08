ccfa14750fcadf0f697f8d99e7a0b142b62f945f
saisaran16 <nsvvsaisaran@gmail.com>
2020-07-08 12:36:05 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,4 +3,4 @@ exports.client_secret = "ZGE4MTdlZTUtMWQ3MS00";
 exports.authorization_url = "https://preprod.login.w3.ibm.com/oidc/endpoint/default/authorize";
 exports.token_url = "https://preprod.login.w3.ibm.com/oidc/endpoint/default/token";
 exports.issuer_id = "https://preprod.login.w3.ibm.com/oidc/endpoint/default";
-exports.callback_url = "https://hello-world12345asdfg.w3ibm.mybluemix.net/";      
+exports.callback_url = "https://hello-world12345asdfg.w3ibm.mybluemix.net/auth/sso/callback";      
